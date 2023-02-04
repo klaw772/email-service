cd5f318dff4b64ffb4e3d9bf65652c189205050d
Kat Law <kathleen.law@multiverse.io>
2023-02-03 21:32:17 -0500

    query params

--- handler.js
+++ handler.js
@@ -5,6 +5,19 @@ const ses = new AWS.SES({ region: 'us-east-1' });
 'use strict';
 
 module.exports.sendEmail = async (event) => {
+  const queryParams = event.queryStringParameters || {};
+  // select from the query parameters
+  let { email, message, subject } = queryParams;
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Email is required',
+      }),
+    };
+  }
+  message = message || 'This is a message generated automatically from a Lambda function.';
+  subject = subject || 'Hello from Lambda';
   const params = {
     Destination: {
       ToAddresses: ['kathleen.law@multiverse.io'], // This should be your email address
@@ -22,12 +35,13 @@ module.exports.sendEmail = async (event) => {
     Source: 'kathleen.law@multiverse.io', // This can be any email address, the email you want to show as the "sender" when the email is received
   };
   await ses.sendEmail(params).promise();
+  
   // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `Email sent to ${queryParams.email}`,
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
         input: event,
       },
       null,

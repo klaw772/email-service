338e937fff146a6e69aba052de794e2f8da767a8
Kat Law <kathleen.law@multiverse.io>
2023-02-03 21:16:49 -0500

    email setup

--- handler.js
+++ handler.js
@@ -1,11 +1,33 @@
+// top of file
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'us-east-1' });
+
 'use strict';
 
 module.exports.sendEmail = async (event) => {
+  const params = {
+    Destination: {
+      ToAddresses: ['kathleen.law@multiverse.io'], // This should be your email address
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'This is a message generated automatically from a Lambda function.',
+        },
+      },
+      Subject: {
+        Data: 'Hello from Lambda',
+      },
+    },
+    Source: 'example@hello.com', // This can be any email address, the email you want to show as the "sender" when the email is received
+  };
+  await ses.sendEmail(params).promise();
+  // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${queryParams.email}`
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: `Email sent to ${queryParams.email}`,
         input: event,
       },
       null,
